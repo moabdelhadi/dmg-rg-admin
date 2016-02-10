a949acd852d9c698422d6c999669fb04268d4cec
Mohammad Abdelhadi <m.abdelhadi@gmail.com>
2016-02-10 15:41:41 +0400

    Sort

--- src/main/java/com/dmg/admin/view/TransactionsView.java
+++ src/main/java/com/dmg/admin/view/TransactionsView.java
@@ -1,12 +1,8 @@
 package com.dmg.admin.view;
 
 import java.util.Date;
-import java.util.List;
-
-import org.apache.poi.ss.usermodel.CellStyle;
 
 import com.dmg.admin.auth.SessionHandler;
-import com.dmg.admin.service.TransactionService;
 import com.dmg.admin.ui.ComponentUtil;
 import com.dmg.admin.ui.CustomFilterDecorator;
 import com.dmg.admin.ui.CustomFilterGenerator;
@@ -14,20 +10,15 @@ import com.dmg.admin.ui.CustomPagedFilterControlConfig;
 import com.dmg.admin.ui.CustomPagedFilterTable;
 import com.dmg.admin.util.ViewUtil;
 import com.dmg.core.bean.ApproveStatusEnum;
-import com.dmg.core.bean.BillAUH;
-import com.dmg.core.bean.BillDu;
 import com.dmg.core.bean.Transaction;
 import com.dmg.core.bean.TransactionAUH;
 import com.dmg.core.bean.TransactionDu;
-import com.dmg.core.bean.UserAccount;
-import com.dmg.core.bean.UserAccountsAUH;
-import com.dmg.core.bean.UserAccountsDU;
-import com.dmg.core.exception.DataAccessLayerException;
 import com.vaadin.addon.jpacontainer.JPAContainer;
 import com.vaadin.addon.jpacontainer.JPAContainerFactory;
 import com.vaadin.addon.tableexport.CustomTableHolder;
 import com.vaadin.addon.tableexport.ExcelExport;
-import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.data.Container.ItemSetChangeEvent;
+import com.vaadin.data.Container.ItemSetChangeListener;
 import com.vaadin.event.ItemClickEvent;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.navigator.View;
@@ -41,32 +32,33 @@ import com.vaadin.ui.CustomTable;
 import com.vaadin.ui.CustomTable.ColumnGenerator;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.Notification.Type;
 import com.vaadin.ui.VerticalLayout;
 
 public class TransactionsView extends VerticalLayout implements View {
 
+	private static final int EXPORT_LIMIT = 5000;
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 8536891631036164352L;
-
 	public static final String NAME = "transactions";
 	private CustomPagedFilterTable pagedTable;
-	private List<Transaction> transactions;
-	private final TransactionService transactionService;
-	BeanItemContainer<Transaction> container = new BeanItemContainer<Transaction>(Transaction.class);
+
+	JPAContainer jpaContainer;
+	//private List<Transaction> transactions;
 	private ExcelExport excelExport;
 	private final Navigator navigator;
 	private Button editBtn;
 	private String city = "";
-	JPAContainer jpaContainer;
+	Button exportButton = new Button("Export");
+
+	//	private final TransactionService transactionService;
+//	BeanItemContainer<Transaction> container = new BeanItemContainer<Transaction>(Transaction.class);
 
 	public TransactionsView(Navigator navigator) {
 		this.navigator = navigator;
 		initView();
-		transactionService = new TransactionService();
+//		transactionService = new TransactionService();
 		setSizeFull();
 		setSpacing(true);
 	}
@@ -75,17 +67,17 @@ public class TransactionsView extends VerticalLayout implements View {
 		city = SessionHandler.get().getCity();
 		if ("DUBAI".equals(city)) {
 			jpaContainer = JPAContainerFactory.makeBatchable(TransactionDu.class, "dmg-rg-admin");
+			jpaContainer.sort(new String[]{"creationDate"}, new boolean[]{false});
 		} else {
 			jpaContainer = JPAContainerFactory.makeBatchable(TransactionAUH.class, "dmg-rg-admin");
+			jpaContainer.sort(new String[]{"creationDate"}, new boolean[]{false});
 		}
+		
 		pagedTable.setContainerDataSource(jpaContainer);
 		pagedTable.setCaption("TransAction Found found (" + jpaContainer.size() + ")");
-		pagedTable.setVisibleColumns("contractNo", "status", "creationDate", "updateDate", "merchTxnRef", "receiptNo", "doubleAmount", "approveStatus");
+		pagedTable.setVisibleColumns("contractNo", "status", "creationDate", "updateDate", "merchTxnRef", "receiptNo", "doubleAmount", "approveStatusEnum");
 	}
 	
-	
-	
-
 	public void initView() {
 		pagedTable = new CustomPagedFilterTable();
 		pagedTable.setFilterDecorator(new CustomFilterDecorator());
@@ -94,7 +86,7 @@ public class TransactionsView extends VerticalLayout implements View {
 		pagedTable.setImmediate(true);
 
 		pagedTable.addContainerProperty("contractNo", String.class, null);
-		pagedTable.addContainerProperty("city", String.class, null);
+//		pagedTable.addContainerProperty("city", String.class, null);
 		pagedTable.addContainerProperty("status", String.class, null);
 		pagedTable.addContainerProperty("creationDate", Date.class, null);
 		pagedTable.addContainerProperty("updateDate", Date.class, null);
@@ -104,7 +96,7 @@ public class TransactionsView extends VerticalLayout implements View {
 		pagedTable.addContainerProperty("approveStatusEnum", ApproveStatusEnum.class, null);
 
 		pagedTable.setColumnHeader("contractNo", "Contract #.");
-		pagedTable.setColumnHeader("city", "City");
+//		pagedTable.setColumnHeader("city", "City");
 		pagedTable.setColumnHeader("status", "Status");
 		pagedTable.setColumnHeader("creationDate", "Creation Date");
 		pagedTable.setColumnHeader("updateDate", "Update Date");
@@ -113,11 +105,24 @@ public class TransactionsView extends VerticalLayout implements View {
 		pagedTable.setColumnHeader("doubleAmount", "Amount");
 		pagedTable.setColumnHeader("approveStatusEnum", "Approve Status");
 
-		pagedTable.setContainerDataSource(container);
+//		pagedTable.setContainerDataSource(container);
 		pagedTable.setSizeFull();
 
-		pagedTable.setVisibleColumns("contractNo", "status", "creationDate", "updateDate", "merchTxnRef","receiptNo", "doubleAmount", "approveStatusEnum");
+//		pagedTable.setVisibleColumns("contractNo",  "status", "creationDate", "updateDate", "merchTxnRef","receiptNo", "doubleAmount", "approveStatusEnum");
 
+		pagedTable.addItemClickListener(new ItemClickEvent.ItemClickListener() {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+
+			@Override
+			public void itemClick(ItemClickEvent itemClickEvent) {
+				editBtn.setVisible(true);
+				editBtn.setDescription("Click here to edit a transaction!");
+			}
+		});
+		
 		pagedTable.addGeneratedColumn("approveStatusEnum", new ColumnGenerator() {
 
 			/**
@@ -137,9 +142,9 @@ public class TransactionsView extends VerticalLayout implements View {
 					txn = (TransactionAUH) jpaContainer.getItem(itemId).getEntity();
 				}
 				
-				
 				Label label = new Label();
 				switch (txn.getApproveStatusEnum()) {
+
 				case APPROVED:
 					label.setCaption(ApproveStatusEnum.APPROVED.getName());
 					label.setIcon(new ThemeResource("img/approved.png"));
@@ -175,6 +180,22 @@ public class TransactionsView extends VerticalLayout implements View {
 		HorizontalLayout buttonLayout = new HorizontalLayout();
 		buttonLayout.setWidth("100%");
 
+		pagedTable.addItemSetChangeListener(new ItemSetChangeListener() {
+
+			@Override
+			public void containerItemSetChange(ItemSetChangeEvent event) {
+				int itemSize = jpaContainer.size();
+				pagedTable.setCaption("Txns found (" + itemSize + ")");
+				if (itemSize > EXPORT_LIMIT) {
+					exportButton.setVisible(false);
+				} else {
+					exportButton.setVisible(true);
+				}
+
+			}
+		});
+		
+		
 		editBtn = new Button("Edit");
 		editBtn.setIcon(new ThemeResource(ViewUtil.EDIT_ICON));
 
@@ -192,20 +213,6 @@ public class TransactionsView extends VerticalLayout implements View {
 
 		});
 
-		pagedTable.addItemClickListener(new ItemClickEvent.ItemClickListener() {
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-
-			@Override
-			public void itemClick(ItemClickEvent itemClickEvent) {
-				editBtn.setVisible(true);
-				editBtn.setDescription("Click here to edit a transaction!");
-			}
-		});
-
-		Button exportButton = new Button("Export");
 		exportButton.setIcon(new ThemeResource(ViewUtil.EXCEL_ICON));
 		exportButton.addClickListener(new ClickListener() {
 
@@ -246,21 +253,21 @@ public class TransactionsView extends VerticalLayout implements View {
 		setExpandRatio(pagerControls, 0.05F);
 	}
 
-	private void reloadResult() {
-		try {
-			transactions = transactionService.listTransactions();
-		} catch (DataAccessLayerException e) {
-			Notification.show("Error", "There was a DB error while retrieving transactions please contact help disk", Type.ERROR_MESSAGE);
-		}
-		container.removeAllItems();
-		for (Transaction transaction : transactions) {
-			container.addBean(transaction);
-		}
-		pagedTable.setCurrentPage(1);
-		pagedTable.setCaption("Events found (" + transactions.size() + ")");
-		pagedTable.setImmediate(true);
-
-	}
+//	private void reloadResult() {
+//		try {
+//			transactions = transactionService.listTransactions();
+//		} catch (DataAccessLayerException e) {
+//			Notification.show("Error", "There was a DB error while retrieving transactions please contact help disk", Type.ERROR_MESSAGE);
+//		}
+//		container.removeAllItems();
+//		for (Transaction transaction : transactions) {
+//			container.addBean(transaction);
+//		}
+//		pagedTable.setCurrentPage(1);
+//		pagedTable.setCaption("Events found (" + transactions.size() + ")");
+//		pagedTable.setImmediate(true);
+//
+//	}
 
 	@Override
 	public void enter(ViewChangeEvent event) {
